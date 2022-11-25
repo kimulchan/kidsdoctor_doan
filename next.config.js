6f19e040bfa13c884c4a7b33ad845311161740fb
kimuichan <leerabi577@gmail.com>
2022-11-25 11:53:41 +0900

    .

--- next.config.js
+++ next.config.js
@@ -2,14 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
-    return [
-      {
-        source: "/",
-        destination: "/naver8ac868d8da74002d5f2ac6df2e6a250e.html",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/robots.txt",
+  //       destination: "/api/robots",
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,44 +3,29 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { global } from "../styles/global";
 import { theme } from "../styles/theme";
-import { DefaultSeo } from "next-seo";
 function MyApp({ Component, pageProps }: AppProps) {
-  const DEFAULT_SEO = {
-    title: "키즈닥터 도안원",
-    description: "창의력 영재교육 키즈닥터 도안원입니다.",
-    canonical: "https://kidsdoctor-doan.vercel.app/",
-    openGraph: {
-      type: "website",
-      locale: "ko_KR",
-      url: "https://kidsdoctor-doan.vercel.app/",
-      title: "키즈닥터 도안원",
-      site_name: "키즈닥터 도안원",
-      images: [
-        {
-          url: "../assets/common/backgroundImage.png",
-          width: 285,
-          height: 167,
-          alt: "배경 이미지",
-        },
-      ],
-    },
-    twitter: {
-      handle: "@handle",
-      site: "@site",
-      cardType: "summary_large_image",
-    },
-  };
   return (
     <>
       <Head>
         <title>키즈닥터 도안원</title>
-        <DefaultSeo {...DEFAULT_SEO}></DefaultSeo>
+        <meta name="robots" content="index,follow"></meta>
         <meta
           name="naver-site-verification"
           content="96658fc3919f72e4f459c227efa5c9677e8c680e"
         />
+        <meta
+          name="google-site-verification"
+          content="4RO3tyHMN_Qu_bOqq52x_9Zb3iz33se5jTfmPGyZXKg"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="키즈닥터 도안원" />
+        <meta property="og:description" content="키즈닥터 도안원입니다." />
+        <meta
+          property="og:image"
+          content="https://kidsdoctor-doan.vercel.app/backgroundImage.png"
+        />
+        <meta property="og:url" content="https://kidsdoctor-doan.vercel.app" />
       </Head>
-
       <ThemeProvider theme={theme}>
         <Global styles={global}></Global>
         <Component {...pageProps} />
