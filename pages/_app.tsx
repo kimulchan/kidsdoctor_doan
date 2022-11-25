296f133bba7b4f0c547d7957c46d759f65b556d0
kimuichan <leerabi577@gmail.com>
2022-11-25 12:03:27 +0900

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,10 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="google-site-verification"
           content="4RO3tyHMN_Qu_bOqq52x_9Zb3iz33se5jTfmPGyZXKg"
         />
+        <meta
+          name="description"
+          content="키즈닥터 도안원 홈페이지 입니다."
+        ></meta>
         <meta property="og:type" content="website" />
         <meta property="og:title" content="키즈닥터 도안원" />
         <meta property="og:description" content="키즈닥터 도안원입니다." />
