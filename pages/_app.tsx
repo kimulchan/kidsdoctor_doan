b56cffcd43fb51a37c5a7badc21c116b4158faf4
kimulchan <leerabi577@gmail.com>
2023-08-02 10:06:17 +0900

    feat: keyword 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>키즈닥터 도안 영재학원</title>
+        <title>키즈닥터 도안원</title>
         <meta name="robots" content="index,follow"></meta>
         <meta
           name="naver-site-verification"
@@ -20,7 +20,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="description" content="생각 공작소 키즈닥터 도안 영재학원" />
         <meta
           name="keywords"
-          content="키즈닥터, 키즈닥터 도안, 도안동 학원, 창의력 학원, 유아 학원, 학원, 초등학생 학원, 과학실험, 수학학원, 보드게임 학원, kidsdoctor, 도안 초등학교, 도안초, 학부모, 도안, 도안 학원"
+          content="키즈닥터, 키즈닥터 도안, 키즈닥터 도안 영재학원, 도안동 학원, 과학실험, 도안 수학학원, 도안 보드게임 학원, kidsdoctor, 도안 초등학교, 도안초, 학부모, 도안, 도안 학원"
         />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="키즈 닥터 도안 영재학원" />
